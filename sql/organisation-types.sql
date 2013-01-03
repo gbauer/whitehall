60ace77fc547f0dae3b5271ff50717e6273ff0b5
gbauer <gernot.bauer@geroba.at>
2013-01-03 13:35:10 +0100

    Add test data sql scripts

---
+++ sql/organisation-types.sql
@@ -0,0 +1,2 @@
+-- SET IDENTITY_INSERT organisation_types ON;
+insert into organisation_types (id, name) values (1, 'Ministerial department'), (2, 'Non-ministerial department'), (3, 'Public corporation')
